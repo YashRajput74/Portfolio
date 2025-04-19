621231da28defdb7f8422faa6c3c47b1f6060f36
Yash Rajput <yashrajput7455@gmail.com>
2025-04-19 13:14:00 +0530

    added js for animated text

---
+++ main.js
@@ -0,0 +1,35 @@
+function animateText(){
+    const textElement=document.querySelector(".animatedText");
+    const words=["Developer.","Web Developer."];
+    let wordIndex=0;
+    let charIndex=0;
+    let isDeleting=false;
+
+    function startTyping(){
+        const currentWord=words[wordIndex];
+        const currentText=currentWord.substring(0,charIndex);
+        textElement.textContent=currentText;
+
+        if(!isDeleting && charIndex<currentWord.length){
+            charIndex++;
+            setTimeout(startTyping,700);
+        }
+        else if(isDeleting && charIndex>0){
+            charIndex--;
+            setTimeout(startTyping,400)
+        }
+        else{
+            isDeleting=!isDeleting;
+            if(!isDeleting) wordIndex++;
+            if(wordIndex>=words.length) wordIndex=0;
+            setTimeout(startTyping,2000)
+        }
+    }
+    startTyping();
+}
+
+(function bindEvents(){
+    document.addEventListener("DOMContentLoaded",()=>{
+        setTimeout(animateText,1000);
+    })
+})();
